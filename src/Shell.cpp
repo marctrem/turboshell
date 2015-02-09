2dd6ecc3692299fcd5c0da45ae49028d20e4dc9f
Marc Tremblay <marcandre.tr@gmail.com>
2015-02-09 17:40:49 -0500

    Add: Basic input tokenization

--- src/Shell.cpp
+++ src/Shell.cpp
@@ -1,12 +1,66 @@
 #include "Shell.hpp"
 
-Shell::Shell(FILE * in, FILE * out, FILE * err) {
+#include <boost/tokenizer.hpp>
+
+const std::string PROMPT_MSG = "TSH> ";
+
+Shell::Shell(std::istream *in, FILE *out, FILE *err) {
     this->in = in;
     this->out = out;
     this->err = err;
+
+    this->cwd = boost::filesystem::current_path();
 }
 
 int Shell::run() {
 
+    int lastStatus = 0;
+
+    do {
+        auto linetokens = std::list<std::string>();
+
+        this->displayPrompt();
+        lastStatus &= this->tokenizeInput(linetokens);
+        lastStatus &= this->processInput(linetokens);
+
+
+    }
+    while(!lastStatus);
+
     return 0;
+}
+
+void Shell::displayPrompt() {
+    std::cout << "\n" << " " << PROMPT_MSG;
+}
+
+int Shell::tokenizeInput(std::list<std::string> &tokens) {
+    /*
+    Takes the input and puts it's token in the provided list.
+     */
+    std::string line;
+
+    if(std::getline(*this->in, line)) {
+        boost::tokenizer<boost::char_separator<char>> tok(line);
+
+        for(auto it = tok.begin(); it != tok.end(); it++) {
+            tokens.push_back(*it);
+        }
+        return 0;
+    }
+    else {
+        // Input failed.
+        return -1;
+    }
+}
+
+int Shell::processBuiltin(std::list<std::string> &tokens) {
+    // TODO: Process builtins.
+}
+
+int Shell::processInput(std::list<std::string> &tokens) {
+
+
+    return 0;
+
 }
--- src/Shell.hpp
+++ src/Shell.hpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include <list>
+#include <boost/filesystem.hpp>
 
 class Shell {
 
 public:
 
-    Shell(FILE* in=stdin, FILE* out=stdout, FILE* err=stderr);
+    Shell(std::istream *in=&std::cin, FILE *out=stdout, FILE *err=stderr);
 
     int run();
 
 private:
-    FILE* in;
+    std::istream *in;
     FILE* out;
     FILE* err;
 
+    boost::filesystem::path cwd;
+
+
+    void displayPrompt();
+    int tokenizeInput(std::list<std::string> &tokens);
+    int processInput(std::list<std::string> &tokens);
+    int processBuiltin(std::list<std::string> &tokens);
 };
