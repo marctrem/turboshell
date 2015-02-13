8629e6e4718e33c3c1cecdf2e545260b94a01d33
Marc Tremblay <marcandre.tr@gmail.com>
2015-02-13 16:29:15 -0500

    Migration from boost to libpathm

--- tshlist/src/main.cpp
+++ tshlist/src/main.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
-#include <boost/filesystem.hpp>
+#include <list>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "../../libpathm/include/path.hpp"
 
 using namespace std;
-
-namespace fs = boost::filesystem;
-
+using namespace pathm;
 
 int main(int argc, char *argv[]) {
 
-    fs::path cwd = boost::filesystem::current_path();
+    path cwd = path::get_current_path();
 
     bool showDirectories;
 
     // Todo: Make it tougher.
     if (argc > 1 && !strncmp(argv[1], "-d", 2)) {
-        cout << "Sous-répertoires de " << cwd.string() << endl;
+        cout << "Sous-répertoires de " << cwd << endl;
         showDirectories = true;
     }
     else {
-        cout << "Fichiers de " << cwd.string() << endl;
+        cout << "Fichiers de " << cwd << endl;
         showDirectories = false;
     }
 
-    for(auto it = fs::directory_iterator(cwd); it != fs::range_end(it); it++) {
-        if (showDirectories && fs::is_directory(*it)) {
-            cout << it->path().filename().string() << endl;
-        }
-        else if (!showDirectories && fs::is_regular_file(*it)) {
-            cout << it->path().filename().string() << endl;
+    auto dir_listing = cwd.list_directory();
+
+    for (auto entry : dir_listing) {
+        auto full_path = cwd;
+        full_path.append(entry.d_name);
+
+        if (showDirectories and full_path.is_a(S_IFDIR)
+                or !showDirectories and full_path.is_a(S_IFREG)) {
+            std::cout << entry.d_name << std::endl;
         }
     }
+
     return 0;
 }
